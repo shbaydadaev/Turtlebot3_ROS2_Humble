efe92b24d72f0f0637279ad0bf555d2804a19f24
shbaydadaev <sh.baydadaev@inha.edu>
2025-06-24 23:02:26 +0900

    updated shell file

--- install_ros2_humble.sh
+++ install_ros2_humble.sh
@@ -17,6 +17,7 @@ name_os_version=${name_os_version:="jammy"}
 name_ros_version=${name_ros_version:="humble"}
 name_colcon_workspace=${name_colcon_workspace:="turtlebot3_ws"}
 
+# Prerequisite Setup
 echo "[Install prerequisites]"
 sudo apt update && sudo apt install -y locales 
 sudo locale-gen en_US en_US.UTF-8
@@ -26,15 +27,20 @@ locale
 sudo apt install -y software-properties-common
 sudo add-apt-repository universe
 
+# ROS 2 Apt Source Setup
+echo "[ROS 2 Apt Source Setup]"
 sudo apt update && sudo apt install curl -y
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb"
 sudo apt install /tmp/ros2-apt-source.deb
 
+# ROS 2 & Package Installation
+echo "[ROS 2 & Package Installation]"
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y ros-${name_ros_version}-desktop
 
+# Development Tools & Dependencies
 echo "[Environment setup and development tools]"
 source /opt/ros/${name_ros_version}/setup.bash
 sudo apt install -y python3-colcon-common-extensions \
@@ -46,16 +52,18 @@ sudo apt install -y python3-colcon-common-extensions \
                     net-tools \
                     x11-apps \
                     iputils-ping \
-                    ros-${name_ros_version}-gazebo-ros-pkgs \
+                    ros-${name_ros_version}-gazebo-* \
                     ros-${name_ros_version}-cartographer \
                     ros-${name_ros_version}-cartographer-ros \
                     ros-${name_ros_version}-navigation2 \
                     ros-${name_ros_version}-nav2-bringup
 
+# Initialize rosdep
 echo "[Create and build the colcon workspace]"
 mkdir -p $HOME/$name_colcon_workspace/src
 cd $HOME/$name_colcon_workspace/src
 
+# Cloning TurtleBot3 repositories
 echo "📥 Cloning TurtleBot3 repositories..."
 git clone -b ${name_ros_version} https://github.com/ROBOTIS-GIT/DynamixelSDK.git 
 git clone -b ${name_ros_version} https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git 
@@ -64,25 +72,32 @@ git clone -b ${name_ros_version} https://github.com/ROBOTIS-GIT/turtlebot3_simul
 git clone -b ${name_ros_version} https://github.com/ROBOTIS-GIT/turtlebot3_applications.git 
 git clone -b ${name_ros_version} https://github.com/ROBOTIS-GIT/turtlebot3_applications_msgs.git
 
+# building the workspace
+echo "[Build the colcon workspace]"
 cd $HOME/$name_colcon_workspace
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+colcon build --symlink-install
 
+# Setup rosdep
+# ------------------------------
+
+# Alias Section
 echo "[Setup user environment]"
-{
-    echo "source /opt/ros/$name_ros_version/setup.bash"
-    echo "source \$HOME/$name_colcon_workspace/install/setup.bash"
-    echo "source /usr/share/gazebo/setup.sh"
-    echo "alias nb='nano ~/.bashrc'"
-    echo "alias sb='source ~/.bashrc'"
-    echo "alias cw='cd ~/$name_colcon_workspace'"
-    echo "alias cs='cd ~/$name_colcon_workspace/src'"
-    echo "alias cb='cd ~/$name_colcon_workspace && colcon build'"
-    echo "export TURTLEBOT3_MODEL=burger"
-    echo "export ROS_DOMAIN_ID=30"
-    echo "export RMW_IMPLEMENTATION=rmw_fastrtps_cpp"
-} >> ~/.bashrc
+echo "alias nb='nano ~/.bashrc'" >> ~/.bashrc
+echo "alias sb='source ~/.bashrc'" >> ~/.bashrc
+echo "alias cw='cd ~/$name_colcon_workspace'" >> ~/.bashrc
+echo "alias cs='cd ~/$name_colcon_workspace/src'" >> ~/.bashrc
+echo "alias cb='cd ~/$name_colcon_workspace && colcon build'" >> ~/.bashrc
+
+# Environment Setup
+echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
+echo "export ROS_DOMAIN_ID=30" >> ~/.bashrc
+echo "export RMW_IMPLEMENTATION=rmw_fastrtps_cpp" >> ~/.bashrc
+echo "source /opt/ros/$name_ros_version/setup.bash" >> ~/.bashrc
+echo "source \$HOME/$name_colcon_workspace/install/setup.bash" >> ~/.bashrc
+echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
+
+# DISPLAY Variables
 echo 'export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk "{print \$2}"):0' >> ~/.bashrc
-export DISPLAY=host.docker.internal:0.0
 
 source ~/.bashrc
 echo "[✅ ROS source setup!]"
